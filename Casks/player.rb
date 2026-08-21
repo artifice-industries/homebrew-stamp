2052e99d996c21053da70b0f4291b1afb980c143
Stamp Release <srsmallman@mac.com>
2026-08-21 09:28:42 +0100

    Player 0.12.0 (138)

--- Casks/player.rb
+++ Casks/player.rb
@@ -1,8 +1,8 @@
 cask "player" do
-  version "0.11.0,137"
-  sha256 "40e2e209f004bf0c49ba9ad27f416f0b680ce6a95133e2b6536839441cc246a7"
+  version "0.12.0,138"
+  sha256 "8a75ae8060f1720954dfe7d550747c9efbd39deaebcdce8b52c8d251e2a0ea8d"
 
-  url "https://github.com/artifice-industries/homebrew-stamp/releases/download/v0.11.0-137/StampPlayer-0.11.0.dmg"
+  url "https://github.com/artifice-industries/homebrew-stamp/releases/download/v0.12.0-138/StampPlayer-0.12.0.dmg"
   name "Stamp Player"
   desc "View and inspect Stamp recordings"
   homepage "https://stamp.xyz/"
